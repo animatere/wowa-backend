90269c79c5b3575a648926316a1bc5ef2af195f2
Alex Wolf <animatere@gmail.com>
2023-03-10 19:56:00 +0100

    removed mongodb credentials

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://alexwolf:P92GBeQ1gqPsjs1P@wowa-cluster.nlhp8ob.mongodb.net/test')],
+  imports: [MongooseModule.forRoot('mongodb+srv://<username>:<password>@wowa-cluster.nlhp8ob.mongodb.net/test')],
   controllers: [AppController],
   providers: [AppService],
 })
